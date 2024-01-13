e3e137be8347ef7e7be6c3225bec75d325af3a81
Robert Lewis <Robbysim37@gmail.com>
2024-01-13 13:34:44 -0500

    gamestate matrix reaches each cell

--- src/app/game-state/game-state.component.ts
+++ src/app/game-state/game-state.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './game-state.component.css'
 })
 export class GameStateComponent {
-
+  gameBoard = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+  ]
 }
