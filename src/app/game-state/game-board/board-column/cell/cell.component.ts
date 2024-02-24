7ae1cf7599cc09e64170e592b3c6c710e1f0688b
Robert Lewis <Robbysim37@gmail.com>
2024-02-24 15:07:59 -0500

    dice now load images

--- src/app/game-state/game-board/board-column/cell/cell.component.ts
+++ src/app/game-state/game-board/board-column/cell/cell.component.ts
@@ -1,10 +1,16 @@
-import { Component,Input } from '@angular/core';
+import { Component,Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-cell',
   templateUrl: './cell.component.html',
   styleUrl: './cell.component.css'
 })
-export class CellComponent {
+export class CellComponent implements OnChanges {
   @Input() diceValue: number | null = null;
+  imgPath:string = ``
+  
+
+ngOnChanges(changes: SimpleChanges): void {
+  this.imgPath = `assets/dice/White_D${this.diceValue}.png`
+}
 }
